c451247527a6c634e68ed5d058dfe79f9f3158cb
ayoub2hassani <ayoub2hassani@gmail.com>
2025-06-18 15:46:39 +0200

    Remove frontend_src folder

--- frontend_src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/footer.css'
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2025 AurorAI — All rights reserved</p>
-    </footer>
-  )
-}
